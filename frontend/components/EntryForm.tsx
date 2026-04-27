8ddce39f273833afc410344605ea65282f19c93c
BrandonEastwell <brandoneastwell@yahoo.com>
2026-04-27 18:57:26 +0100

    added fixed submit button to form

--- frontend/components/EntryForm.tsx
+++ frontend/components/EntryForm.tsx
@@ -4,14 +4,17 @@ import {useState} from "react";
 export default function EntryForm() {
     const [content, setContent] = useState<undefined | string>()
 
-    const handleSave = () => {
-
+    const handleSave = (event: React.SubmitEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        console.log(formData.get("content") as string)
     }
 
     return (
-        <form onSubmit={handleSave}>
+        <form id="journal-entry-form"
+            onSubmit={handleSave}>
                         <textarea name="content"
-                                  className="min-h-[300px] font-mono w-full text-sm text-black placeholder-black"
+                                  className="min-h-[300px] h-auto font-mono w-full text-sm text-black placeholder-black"
                                   value={content}
                                   placeholder="How was your day?"
                                   onChange={(e) => setContent(e.target.value)}>
