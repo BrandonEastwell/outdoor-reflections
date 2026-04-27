f9aad7fcacc00595790a75cb7cee6165d6d4091a
BrandonEastwell <brandoneastwell@yahoo.com>
2026-04-28 00:33:07 +0100

    add backend to repo

---
+++ backend/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {ReflectionsController} from "./reflections.controller";
+import {ReflectionsService} from "./reflections.service";
+
+@Module({
+  imports: [],
+  controllers: [AppController, ReflectionsController],
+  providers: [AppService, ReflectionsService],
+})
+export class AppModule {}
---
+++ backend/src/interfaces/reflection.interface.ts
@@ -0,0 +1,4 @@
+export interface Reflection {
+    title: string
+    content: string
+}
