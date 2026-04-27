cf9a2f303cb3f138fb9cc0d670ebb1374ba86d4b
BrandonEastwell <brandoneastwell@yahoo.com>
2026-04-27 18:57:26 +0100

    added fixed submit button to form

--- frontend/app/entry/layout.tsx
+++ frontend/app/entry/layout.tsx
@@ -3,8 +3,13 @@ export default function EntryLayout({ children } : { children: React.ReactNode }
 
 
     return (
-        <div className="bg-[#F9F7F3]">
-            {children}
+        <div className="flex flex-col min-h-screen bg-[#F9F7F3] pt-16 pb-16 px-8">
+            <div>{children}</div>
+            <div className="fixed bottom-0 z-50 px-8 pb-2 right-0">
+                <button type="submit" form="journal-entry-form" className="font-flower text-rose text-2xl">
+                    Save
+                </button>
+            </div>
         </div>
     )
 }
