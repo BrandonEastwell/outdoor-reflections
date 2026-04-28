a8d494873a4e208acfda25c94815fe57e54403b6
BrandonEastwell <brandoneastwell@yahoo.com>
2026-04-28 17:47:22 +0100

    added repository class for db

--- backend/src/reflections/reflections.service.ts
+++ backend/src/reflections/reflections.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from "@nestjs/common";
 import {Reflection} from "../interfaces/reflection.interface";
+import {ReflectionsRepository} from "./reflections.repository";
 
 
 @Injectable()
 export class ReflectionsService {
-    private readonly reflections: Reflection[] = [];
+    constructor(private repo: ReflectionsRepository) {}
 
     createEntry(reflectionEntry: Reflection) {
-        this.reflections.push(reflectionEntry)
+
     }
 }
