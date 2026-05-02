ef916ff5c7b1680b29ca067f33a139f35ad6382a
BrandonEastwell <brandoneastwell@yahoo.com>
2026-05-02 23:56:51 +0100

    modified controllers to use express request object

--- backend/src/reflections/reflections.controller.ts
+++ backend/src/reflections/reflections.controller.ts
@@ -1,14 +1,17 @@
-import {Body, Controller, Get, Param, Post} from "@nestjs/common";
+import {Controller, Get, Param, Post, Req, Res} from "@nestjs/common";
 import {ReflectionsService} from "./reflections.service";
 import {ReflectionDTO} from "../interfaces/reflection.types";
+import type {Request, Response} from 'express';
 
 @Controller('reflection')
 export class ReflectionsController {
     constructor(private reflectionService: ReflectionsService) {}
 
     @Post()
-    async create(@Body() body: ReflectionDTO ) {
-        return this.reflectionService.createEntry(body)
+    async create(@Req() req: Request, @Res() res: Response) {
+        const { title, drawing, content, userID } = req.body;
+        const dto: ReflectionDTO = { title, drawing, content }
+        return this.reflectionService.createEntry(dto, userID)
     }
 
     @Get(':id')
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,19 @@
 import {UserService} from "./user.service";
+import {Controller, Get, Post, Req, Res} from "@nestjs/common";
+import type { Request, Response } from 'express';
 
+@Controller('users')
 export class UserController {
      constructor(private userService: UserService) {}
 
+    @Get(':id')
+    findUser(@Req() req: Request, @Res() res: Response) {
+         const userID: string = req.params.id[0];
+    }
 
+    @Post()
+    createUser(@Req() req: Request, @Res() res: Response) {
+         const body = req.body;
+
+    }
  }
