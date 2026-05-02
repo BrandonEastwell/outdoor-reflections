0e048ec9a15c29ecb526d1a25b3a1de2168a40a9
BrandonEastwell <brandoneastwell@yahoo.com>
2026-05-03 00:31:45 +0100

    added User types and DTO

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,7 +1,20 @@
 import {Injectable} from "@nestjs/common";
 import {UserRepository} from "./user.repository";
+import {UserDTO} from "../interfaces/user.types";
 
 @Injectable()
 export class UserService {
     constructor(private repo: UserRepository) {}
+
+    createUser(UserDTO: UserDTO) {
+        
+    }
+
+    findUserByID(id: number) {
+
+    }
+
+    deleteUser() {
+
+    }
 }
