049d656216ad5f9948a2362dc93c1fb2ac4ace64
BrandonEastwell <brandoneastwell@yahoo.com>
2026-05-03 00:31:45 +0100

    added User types and DTO

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,19 +1,21 @@
 import {UserService} from "./user.service";
 import {Controller, Get, Post, Req, Res} from "@nestjs/common";
 import type { Request, Response } from 'express';
+import {UserDTO} from "../interfaces/user.types";
 
 @Controller('users')
 export class UserController {
      constructor(private userService: UserService) {}
 
     @Get(':id')
-    findUser(@Req() req: Request, @Res() res: Response) {
+    findOne(@Req() req: Request, @Res() res: Response) {
          const userID: string = req.params.id[0];
+         this.userService.findUserByID(Number(userID))
     }
 
     @Post()
-    createUser(@Req() req: Request, @Res() res: Response) {
-         const body = req.body;
-
+    create(@Req() req: Request, @Res() res: Response) {
+         const UserDTO: UserDTO = req.body;
+         this.userService.createUser(UserDTO)
     }
  }
