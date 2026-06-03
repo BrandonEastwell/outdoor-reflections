5b9168d2f566bb390327655ec37fc837c07774cb
BrandonEastwell <brandoneastwell@yahoo.com>
2026-06-03 02:43:02 +0100

    passport local auth strategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import {Module} from "@nestjs/common";
 import {AuthService} from "./auth.service";
 import {UserModule} from "../user/user.module";
+import {LocalStrategy} from "./local.strategy";
+import {PassportModule} from "@nestjs/passport";
 
 @Module({
-    imports: [UserModule],
-    providers: [AuthService],
+    imports: [UserModule, PassportModule],
+    providers: [AuthService, LocalStrategy],
 })
 
 export class AuthModule {}
