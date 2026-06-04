bd1ce3b94619aab5fe9597a3f05372266af11211
BrandonEastwell <brandoneastwell@yahoo.com>
2026-06-04 03:15:55 +0100

    canvas points mouse events for form

--- frontend/components/EntryForm.tsx
+++ frontend/components/EntryForm.tsx
@@ -1,8 +1,10 @@
 "use client"
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function EntryForm() {
     const [content, setContent] = useState<undefined | string>()
+    const [lines, setLines] = useState<(number[])[]>([])
+    const drawAreaRef = useRef<HTMLFormElement | null>(null)
 
     const handleSave = (event: React.SubmitEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -10,8 +12,24 @@ export default function EntryForm() {
         console.log(formData.get("content") as string)
     }
 
+    const relativeCoordinates = (event) => {
+        const boundingRect = drawAreaRef.current?.getBoundingClientRect()
+        if (!boundingRect) return;
+        const x = event.clientX - boundingRect.left;
+        const y = event.clientY - boundingRect.top;
+        return [ x , y ]
+    }
+
+    const handleMouseDown = (event: React.MouseEvent<HTMLFormElement>) => {
+        if (event.button !== 0) return
+        const point = relativeCoordinates(event)
+        point && setLines((prevLines) => [...prevLines, point])
+        console.log(lines)
+        console.log(point)
+    }
+
     return (
-        <form id="journal-entry-form"
+        <form ref={drawAreaRef} onMouseDown={handleMouseDown} id="journal-entry-form"
             onSubmit={handleSave}>
                         <textarea name="content"
                                   className="min-h-[300px] h-auto font-mono w-full text-sm text-black placeholder-black"
