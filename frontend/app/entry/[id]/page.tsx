ef5f9b83455f5b6d492c1db0bef70a3a3a2e29f1
BrandonEastwell <brandoneastwell@yahoo.com>
2026-06-07 02:31:32 +0100

    entry editor loader used in entry page

--- frontend/app/entry/[id]/page.tsx
+++ frontend/app/entry/[id]/page.tsx
@@ -1,11 +1,12 @@
 import EntryEditor from "../../../components/EntryEditor";
+import EntryEditorLoader from "@/components/EntryEditorLoader";
 
 export default function Page({ params }: { params: { id: string } }) {
 
     return (
         <main className="flex flex-col flex-1 items-center">
             <h1 className="max-w-xs text-3xl font-semibold font-flower tracking-wider text-black">Your reflections entry</h1>
-            <EntryEditor />
+            <EntryEditorLoader id={params.id} />
         </main>
     )
 }
