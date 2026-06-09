70a430cd38ef007b2c25531e057d89fa105dd2dc
BrandonEastwell <brandoneastwell@yahoo.com>
2026-06-09 01:25:20 +0100

    sidebar changes

--- frontend/components/SideBar.tsx
+++ frontend/components/SideBar.tsx
@@ -8,11 +8,11 @@ export default function SideBar() {
     const [toggleSidebar, setToggleSidebar] = useState<boolean>(false);
 
     return (
-        <div className={"flex flex-col w-full left-0 h-screen m-0.5 border-r bg-rose/5 border-rose/10" + (toggleSidebar ? " max-w-30" : " max-w-6.5")}>
-            <IconButton svgIconPath={SVG_PATHS.flowerIcon} iconSize={24} strokeWidth={1.5} onClick={() => setToggleSidebar(!toggleSidebar)} />
-            <div className="w-full flex flex-col gap-1 mt-6">
-                <IconButton svgIconPath={SVG_PATHS.reflectionIcon} strokeWidth={2} onClick={() => setToggleSidebar(!toggleSidebar)} />
-                <IconButton svgIconPath={SVG_PATHS.userIcon} strokeWidth={2} onClick={() => {}} />
+        <div className={"flex flex-col w-full mx-auto place-items-center h-screen m-0.5 border-r bg-rose/5 border-rose/10" + (toggleSidebar ? " max-w-30" : " max-w-6")}>
+            <IconButton svgIconPath={SVG_PATHS.flowerIcon} iconSize={28} strokeWidth={1.5} onClick={() => setToggleSidebar(!toggleSidebar)} />
+            <div className="w-full flex flex-col place-items-center gap-1 mt-6">
+                <IconButton svgIconPath={SVG_PATHS.reflectionIcon} iconSize={20} strokeWidth={2} onClick={() => setToggleSidebar(!toggleSidebar)} />
+                <IconButton svgIconPath={SVG_PATHS.userIcon} iconSize={20} strokeWidth={2} onClick={() => {}} />
             </div>
         </div>
     )
