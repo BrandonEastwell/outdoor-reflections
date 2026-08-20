cc482dbadaef1c4654169f98ca10aedf7af903d7
Brandon Eastwell <92370206+BrandonEastwell@users.noreply.github.com>
2026-08-20 18:39:52 +0100

    fix: imports for google strategy

--- backend/src/auth/google.strategy.ts
+++ backend/src/auth/google.strategy.ts
@@ -1,7 +1,6 @@
 import {Injectable} from "@nestjs/common";
 import {PassportStrategy} from "@nestjs/passport";
-import {Strategy} from "passport-jwt";
-import {Profile, VerifyCallback} from "passport-google-oauth20";
+import {Profile, Strategy, VerifyCallback} from "passport-google-oauth20";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
